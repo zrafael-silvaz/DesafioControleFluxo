00618765a7d0c25401bbde04103cc8b58a86ba41
rafael <zrafael.silvaz@gmail.com>
2023-09-16 19:47:42 -0300

    Commit Desafio Java Basico

---
+++ src/com/company/Contador.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.err.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois)
+            throw new ParametrosInvalidosException();
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.printf("Imprimindo o número %d\n",i);
+        }
+    }
+}
---
+++ src/com/company/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class ParametrosInvalidosException extends Exception {
+
+}
